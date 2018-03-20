144766cd060de6d081594b499d5629819f331b9a
Javid Abdullayev <abdullaev.javid@gmail.com>
2018-03-20 21:00:23 +0400

    encrypted user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 const Schema = mongoose.Schema;
+
 
 const UserSchema = new Schema({
     name: String,
@@ -18,3 +21,32 @@ const UserSchema = new Schema({
     },
     created: { type: Date, default: Date.now()  }
 })
+
+UserSchema.pre('save', function(next) {
+    let user = this;
+
+      if (!user.isModified('password')) return next();
+
+      bcrypt.hash(user.password, null, null,  (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+});
+
+
+UserSchema.method.comparePasswords = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.method.jAvatar = function (size) {
+    if (!this.size) size = 200;
+    if (!this.email) {
+        return `https://gravatar.com/avatar/?s${size}&d=retro`;
+    } else {
+        let md5 = crypto.createHash('md5').update(this.email).digest('hex');
+        return `https://gravatar.com/avatar/${md5}?s${size}&d=retro`;
+    }
+};
+
+module.exports = mongoose.model('User', UserSchema);
