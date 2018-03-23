1d676e2e4bb0428a7940cb3f976ca4c0f83d914a
Javid Abdullayev <abdullaev.javid@gmail.com>
2018-03-23 22:25:19 +0400

    fixed bugs

--- frontEnd-Angular/e-commerce/src/app/login/login.component.ts
+++ frontEnd-Angular/e-commerce/src/app/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent implements OnInit {
       if (data['success']) {
         localStorage.setItem('token', data['token']);
         await this.data.getProfile();
-        this.router.navigate(['/']);
+        this.router.navigate(['/profile']);
       } else {
         this.data.error(data['message']);
       }
