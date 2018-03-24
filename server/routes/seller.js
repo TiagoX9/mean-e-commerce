55d1db4c1442fd850d4d6810bf18b99ef380dc1a
Javid Abdullayev <abdullaev.javid@gmail.com>
2018-03-24 21:41:15 +0400

    created upload api to aws

--- server/routes/seller.js
+++ server/routes/seller.js
@@ -4,11 +4,12 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const Product = require('../models/product');
+const checkJWT = require('../middlewares/jwt');
 
 // well that would be better if you go to Amazon web services then creat S3 then make it public and replace these keys (it's an easy procedure)
 const s3 = new aws.S3({
     accessKeyId: 'AKIAJFEMPGDYYWODGSJQ',
-    secretAccessKey: 'AkjJWqK+9PJ9F8i8FROt/5xbo/HDQkfb85kgM4/Kc'
+    secretAccessKey: 'AkjJWqK+9PJ9F8i8FROt/5xbo/HDQkfb85kgM4/K'
 })
 
 
@@ -17,12 +18,31 @@ const upload = multer({
         s3: s3,
         bucket: 'mean-commerce',
         metadata: function(req, file, cb) {
-            cb(null, { fieldName: file.fieldName });
+            cb(null, { fieldName: file.fieldname });
         },
-        metadata: function(req, file, cb) {
-            cb(null, { fieldName: file.fieldName });
-        }
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString())
+          }
     })
 });
+
+
+router.route('/products')
+ .get()
+ .post([checkJWT, upload.single('product_image')], (req, res, next) => {
+     let product = new Product();
+     product.category = req.body.categoryId;
+     product.owner = req.decoded.user._id;
+     product.title = req.body.title;
+     product.description = req.body.description;
+     product.image = req.file.location;
+     product.price = req.body.price;
+     product.save();
+     res.json({
+         success: true,
+         message: 'Successfully posted your product',
+
+     });
+ });
 
 module.exports = router;
