3e3e2e3165c2ab18923decd03895938543d8ffcd
Javid Abdullayev <abdullaev.javid@gmail.com>
2018-03-24 10:47:14 +0400

    added category api

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Category = require('../models/account');
+const Category = require('../models/category');
 
 router.route('/categories')
- .get()
+ .get((req, res, next) => {
+     Category.find({}, (err, category) => {
+         res.json({
+             success: true,
+             message: 'Successfully found categories',
+             category: category,
+         })
+     })
+ })
  .post((req, res, next) => {
     let category = new Category();
     category.name = req.body.category;
