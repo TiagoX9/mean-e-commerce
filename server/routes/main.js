fa43de9ae8f159aeeb64ab2948c8966e546f0c7b
Javid Abdullayev <abdullaev.javid@gmail.com>
2018-03-26 13:34:17 +0400

    scalable database

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../models/category');
+const Product = require('../models/product');
+const async = require('async');
+
+/////////////// testing async ///////////////////
+
+router.get('/test', (req, res, next) => {
+    function abc (callback) {
+        var firstName = 'Javid';
+        callback(null, firstName);
+    }
+
+    function xyz (firstName, callback) {
+        var lastName = 'Abd';
+        console.log(`my firstname is ${firstName} and lastname ${lastName}`);
+    }
+
+    async.waterfall([abc, xyz])
+})
 
 
 router.route('/categories')
@@ -23,4 +41,23 @@ router.route('/categories')
      });
  });
 
+ router.get('/categories/:id', (req, res, next) => {
+     const perPage = 10;
+     Product.find({ category: req.params.id })
+        .populate('category')
+        .exec((err, products) => {
+            Product.count({ category: req.params.id }, (err, totalProducts) => {
+                res.json({
+                    success: true,
+                    message: 'category per page',
+                    products: products,
+                    categoryName: products[0].category.name,
+                    totalProducts: totalProducts,
+                    pages: Math.ceil(totalProducts / perPage)
+                });
+            });
+        });
+ });
+
  module.exports = router;
+
