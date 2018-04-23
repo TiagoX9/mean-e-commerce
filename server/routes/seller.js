2fe2cdb9fcd7f1967cc215e7e436c8bce71cf80b
Javid Abdullayev <abdullaev.javid@gmail.com>
2018-04-23 12:25:02 +0400

    Update seller.js

--- server/routes/seller.js
+++ server/routes/seller.js
@@ -10,6 +10,7 @@ const faker = require('faker');
 
 // well that would be better if you go to Amazon web services then creat S3 then make it public and replace these keys (it's an easy procedure)
 const s3 = new aws.S3({
+    // they are no longer working, sorry lazy fuckasses
     accessKeyId: 'AKIAJFEMPGDYYWODGSJQ',
     secretAccessKey: 'AkjJWqK+9PJ9F8i8FROt/5xbo/HDQkfb85kgM4/K'
 })
